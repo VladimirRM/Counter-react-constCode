a7dff8eb76c3787c3ea1c4d4b62bc623d49ac913
Vladimir RM <shvdrummer@gmail.com>
2023-01-26 19:02:51 +0200

    delete old code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [counters, setCounters] = useState([0, 0, 0]);
 
-export default App
+  const count = (n) => {
+    setCounters(
+      counters.map((v, i) => {
+        if (n === i) {
+          return v + 1;
+        }
+        return v;
+      })
+    );
+  };
+  return (
+    <div>
+      <ul>
+        {[0, 1, 2].map((v) => (
+          <li key={v}>
+            <button onClick={() => count(v)}>
+              you pressed me {counters[v]} times
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
