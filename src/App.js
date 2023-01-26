86cb0138f0494357b47ec88c52b566b555909bef
Vladimir RM <shvdrummer@gmail.com>
2023-01-26 16:29:27 +0200

    create new app for exercise

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import "./App.css";
-import React,{useState} from "react";
+import React from 'react'
 
-function App() {
-  const [counters, setCounters] = useState([0,0,0,0]);
-
-const count =(n)=>{
-  setCounters(counters.map((v,i)=>{
-    if(n===i){
-      return v + 1
-    }
-    return v
-  }))
-}
-
+const App = () => {
   return (
-    <div className="App">
-   <ul>
-      {[0,1,2,3].map(v=>(
-        <li key={v}>
-           <button onClick={()=>count(v)}> 
-           You pressed me {counters[v]} times </button>
-           </li>
-      ))}
-      </ul>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
