86b6d8a7cbc52faeb8565f915b7cd4d2153bc94a
Vladimir RM <shvdrummer@gmail.com>
2023-01-26 16:16:58 +0200

    fin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
+import React,{useState} from "react";
 
 function App() {
-  const [counters, setCounters] = useState([0, 0, 0]);
+  const [counters, setCounters] = useState([0,0,0]);
+
+const count =(n)=>{
+  setCounters(counters.map((v,i)=>{
+    if(n===i){
+      return v + 1
+    }
+    return v
+  }))
+}
+
   return (
     <div className="App">
    <ul>
       {[0,1,2].map(v=>(
-        <li>
-           <button onClick={()=>setCounters(counters + 1)}> 
-           You pressed me {count} times </button>
+        <li key={v}>
+           <button onClick={()=>count(v)}> 
+           You pressed me {counters[v]} times </button>
            </li>
       ))}
       </ul>
