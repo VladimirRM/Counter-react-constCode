a5f166088fb938ae040ea67713e1ccc3af866ccc
Vladimir RM <shvdrummer@gmail.com>
2023-01-26 15:22:49 +0200

    add ul & li in map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-
-  const [counters,setCounters ]= useState([0,0,0])
+  const [counters, setCounters] = useState([0, 0, 0]);
   return (
     <div className="App">
-   
+   <ul>
+      {[0,1,2].map(v=>(
+        <li>
+           <button onClick={()=>setCounters(counters + 1)}> 
+           You pressed me {count} times </button>
+           </li>
+      ))}
+      </ul>
     </div>
   );
 }
