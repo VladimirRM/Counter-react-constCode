b98b057a796c123cbe783fec08a792633e1a1085
Vladimir RM <shvdrummer@gmail.com>
2023-01-27 09:12:15 +0200

    add ul & li

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { useState } from "react";
 
 const App = () => {
   const [counters, setCounters] = useState([0, 0, 0]);
-  return <div></div>;
+
+
+  const counter =(number)=>{
+        setCounters(counters.map((item,index)=>{
+            if(number === index ){
+                return item + 1
+            }
+            return item
+        }))
+  }
+  return <div>
+            <ul>{[0,1,2].map(item=>(
+                <li>
+                    
+                </li>
+            ))}</ul>
+         <button> You pushing me {counters[item]}</button>
+
+  </div>;
 };
 
 export default App;
