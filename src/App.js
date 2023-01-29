578d039d4778550c3d5d3d71d38df37ec73bea7b
Vladimir RM <shvdrummer@gmail.com>
2023-01-29 10:01:03 +0200

    create counter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
  const  [counters,setCounters]=useState([0,0,0])
 
  const counter =(number)=>{
-     setCounters(counters.map(item,index=>{
+     setCounters(counters.map((item,index)=>{
       if(number===index){
         return item + 1
       }
@@ -13,7 +13,17 @@ const App = () => {
      }))
  }
   return (
-    <div>App</div>
+    <div>
+         <ul>
+          {[0,1,2,].map(item=>(
+            <li>
+      <button onClick={()=>counter(item)}>You pushing me {counters[item]} times</button>
+
+            </li>
+          ))}
+      </ul>
+      
+    </div>
   )
 }
 
