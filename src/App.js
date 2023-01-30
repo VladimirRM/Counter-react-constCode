5d2a35baf460102e7686be9f81314cb916e2dc2e
Vladimir RM <shvdrummer@gmail.com>
2023-01-30 13:24:17 +0200

    add state counters

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React ,{useState}from 'react'
+
+const App = () => {
+
+  const [counters,setCounters]=useState([0,0,0])
+  const counter = (number)=>{
+    setCounters
+  }
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default App
