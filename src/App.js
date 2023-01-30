7a26e0db1ee4d4c96f814ac49b4ea4772e55ecf1
Vladimir RM <shvdrummer@gmail.com>
2023-01-30 13:26:31 +0200

    create func counter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React ,{useState}from 'react'
+import { render } from "@testing-library/react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [counters, setCounters] = useState([0, 0, 0]);
+  const counter = (number) => {
+    setCounters(
+      counters.map(item, (index) => {
+        if (number === index) {
+        return item + 1
+        } 
+        return item
+      })
+    );
+  };
+  return <div></div>;
+};
 
-  const [counters,setCounters]=useState([0,0,0])
-  const counter = (number)=>{
-    setCounters
-  }
-  return (
-    <div>
-
-
-    </div>
-  )
-}
-
-export default App
+export default App;
