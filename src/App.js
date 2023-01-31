48380b4aafaee1ab6e6668e3906f0d32ab24c7a6
Vladimir RM <shvdrummer@gmail.com>
2023-01-31 09:17:51 +0200

    Compiled successfully!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  const [counters,setCounters]=useState([0,0,0])
+  const [counters, setCounters] = useState([0, 0, 0]);
 
-  const counter =(number)=>{
-       setCounters(counters.map(item,index=>{
-        if(number===index){
-          return item + 1
+  const counter = (number) => {
+    setCounters(
+      counters.map((item, index) => {
+        if (number === index) {
+          return item + 1;
         }
-        return item
-       }))
-  }
+        return item;
+      })
+    );
+  };
 
   return (
     <div>
-          <ul>
-            {[0,1,2].map(item=>(
-              <li></li>
-            ))}
-          </ul>
-
-      <button on onClick={()=>counter(item)}>You pushing me {counters[item]} times</button>
+      <ul>
+        {[0, 1, 2].map((item) => (
+          <li key={item}>
+            <button on onClick={() => counter(item)}>
+              You pushing me {counters[item]} times
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
