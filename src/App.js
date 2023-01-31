ac9281434eecb1aea84b4dbc46ea0c568a5e3362
Vladimir RM <shvdrummer@gmail.com>
2023-01-31 09:13:16 +0200

    add button conters

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React,{useState} from 'react'
 const App = () => {
   const [counters,setCounters]=useState([0,0,0])
 
+  const counter =()=>{
+
+  }
+
   return (
     <div>
-      
+      <button>You pushing me {counters[item]} times</button>
     </div>
   )
 }
