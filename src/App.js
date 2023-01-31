3f0723eb5b626765ac86cfe68a0304212831d00b
Vladimir RM <shvdrummer@gmail.com>
2023-01-31 09:15:03 +0200

    create func counter

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React,{useState} from 'react'
 const App = () => {
   const [counters,setCounters]=useState([0,0,0])
 
-  const counter =()=>{
-
+  const counter =(number)=>{
+       setCounters(counters.map(item,index=>{
+        if(number===index){
+          return item + 1
+        }
+        return item
+       }))
   }
 
   return (
     <div>
-      <button>You pushing me {counters[item]} times</button>
+      <button on>You pushing me {counters[item]} times</button>
     </div>
   )
 }
